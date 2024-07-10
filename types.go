870c99d3c1c8a8465eed2e359289396fd720cebd
WHOISBEETO <whoisbeto@gmail.com>
2024-07-10 02:16:33 -0600

    [add] auth method

--- types.go
+++ types.go
@@ -2,23 +2,59 @@ package main
 
 import (
 	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	ID       int    `json:"id"`
 	Username string  `json:"username"`
+	Password string `json:"-"`
+	Email string `json:"email"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+type Bound struct {
+	ID       int    `json:"id"`
+	Name string  `json:"name"`
+	Number float64 `json:"number"`
+	OwnerID int `json:"ownerId"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
 
 type createUserRequest struct {
 	Username string `json:"username"`
+	Email string `json:"email"`
+	Password string `json:"password"`
 }
 
-func NewAccount(username string) *User {
+type LoginRequest struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Username string `json:"username"`
+	Token string `json:"token"`
+}
+
+func NewAccount(username string, email string, password string) (*User, error) {
+	
+	encriptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
 	return &User{
 		Username: username,
+		Email: email,
+		Password: string(encriptedPassword),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
-	}
+	}, nil 
+}
+
+func (user *User) ComparePassword(password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)) == nil
 }
